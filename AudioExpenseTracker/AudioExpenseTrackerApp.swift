5905f99fc3ae39b969836d8f6582418a7a46f9c2
BufaXiwang <mr.yangzg17@gmail.com>
2025-06-13 22:12:05 +0800

    Initial Commit

---
+++ AudioExpenseTracker/AudioExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioExpenseTrackerApp.swift
+//  AudioExpenseTracker
+//
+//  Created by Zhigang Yang on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioExpenseTrackerTests/AudioExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioExpenseTrackerTests.swift
+//  AudioExpenseTrackerTests
+//
+//  Created by Zhigang Yang on 13/06/2025.
+//
+
+import Testing
+@testable import AudioExpenseTracker
+
+struct AudioExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
